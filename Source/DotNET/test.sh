071b5906ae705e81b1b010d809c0b21078a9134b
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-17 14:07:29 +0200

    Convenience script for testing

--- Source/DotNET/test.sh
+++ Source/DotNET/test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker run -e "REPOSITORY=https://github.com/dolittle/DotNET.Fundamentals.git" -e "COMMIT=beb7544a44dff9283ba2f1d5c3cc8a567dfffa6c" dolittlebuild/dotnet
+#docker run -it -e "REPOSITORY=https://github.com/dolittle/DotNET.Fundamentals.git" -e "COMMIT=beb7544a44dff9283ba2f1d5c3cc8a567dfffa6c" dolittlebuild/dotnet /bin/bash
